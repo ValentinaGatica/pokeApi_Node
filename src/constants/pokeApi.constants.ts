d70e3a279d3b923f126bb95b012dd8e674a47cd1
ValentinaGatica <valentina.15.gatica@gmail.com>
2020-10-09 10:54:03 -0300

    4

--- src/constants/pokeApi.constants.ts
+++ src/constants/pokeApi.constants.ts
@@ -1,1 +1,3 @@
+//src/constants/pokeApi.constants.ts
 export const PORT = 9001;
+export const WELCOME_MESSAGE = "Welcome to pokeAPI REST by Nya ^^";
--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,5 +1,8 @@
 //src/services/pokemon.service.ts
-import { Request, Response } from "express";  
+import { Request, Response } from "express";
+import { WELCOME_MESSAGE } from "../constants/pokeApi.constants";  
 export class PokeService {                         
-public welcomeMessage(req: Request, res: Response) {                           return res.status(200).send("Welcome to pokeAPI REST by Nya ^^");                         }                       
+public welcomeMessage(req: Request, res: Response) {  
+ res.status(200).send(WELCOME_MESSAGE);
+}
 }
